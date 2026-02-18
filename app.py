99c785158a27aede0f49c04240f511b9900ccf34
tinsubc-droid <tinsubc@gmail.com>
2026-02-18 17:53:20 +0530

    Modify home route to display server status message
    Update home route to return a working server message.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return """
+    <h1>Server is Working ✅</h1>
+    """
