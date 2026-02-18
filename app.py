6d316aa8cbb101f915ca40250cd3951f8ffaa4f8
tinsubc-droid <tinsubc@gmail.com>
2026-02-18 18:51:05 +0530

    Add structure route to return directory info
    Added a new route to return current working directory and template information.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/structure")
+def structure():
+    return {
+        "cwd": os.getcwd(),
+        "files_here": os.listdir(),
+        "templates_exists": os.path.exists("templates"),
+        "templates_content": os.listdir("templates") if os.path.exists("templates") else "NO"
+    }
